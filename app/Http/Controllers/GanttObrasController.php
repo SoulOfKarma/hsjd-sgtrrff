7c15bdca0494d84c08a55bc13119534dfa84f044
SoulOfKarma <gomez.soto.ricardo@gmail.com>
2021-07-27 14:32:23 -0400

    se agrega calendario de obras fix 75%

--- app/Http/Controllers/GanttObrasController.php
+++ app/Http/Controllers/GanttObrasController.php
@@ -9,7 +9,21 @@ use Illuminate\Support\Facades\Log;
 
 class GanttObrasController extends Controller
 {
-    public function verificacionId(){
+    public function GetLastIdObras(){
+        try {
+            $get_all = GanttObras::select("gantt_obras.*")
+            ->orderBy("gantt_obras.id","desc")
+            ->limit(1)
+            ->get();
+            return $get_all;
+            
+        } catch (\Throwable $th) {
+            log::info($th);
+            return false;
+        }
+    }
+
+    public function GetObras(){
         try {
             $get_all = GanttObras::all();
             return $get_all;
@@ -30,9 +44,21 @@ class GanttObrasController extends Controller
       }
     }
 
+    public function PostNSubObra(Request $request){
+        try {
+            $get_all = GanttObras::create($request->all());
+            return true;
+        } catch (\Throwable $th) {
+            log::info($th);
+            return false;
+        }
+    }
+
     public function PostNObraRANull(){
         try {
-            $get_all = GanttObras::whereNull("resourceAsociado")
+            $get_all = GanttObras::select("gantt_obras.*", DB::raw("DATE_FORMAT(gantt_obras.start,'%d/%m/%Y') as starts"),
+            DB::raw("DATE_FORMAT(gantt_obras.end,'%d/%m/%Y') as ends"))
+            ->whereNull("resourceAsociado")
             ->get();
             return $get_all;
         } catch (\Throwable $th) {
