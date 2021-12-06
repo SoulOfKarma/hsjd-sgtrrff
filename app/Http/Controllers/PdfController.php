64a2684c0fff3e31b39b94bfc52ba3b4e4357a3d
SoulOfKarma <gomez.soto.ricardo@gmail.com>
2021-12-06 16:05:41 -0300

    ajuste 3

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -246,171 +246,178 @@ class PdfController extends Controller
 
     public function imprimirPorTicketEM($id)
     {
-        $data = GestionTicketEMS::select(
-            'gestion_ticket_e_m_s.*',
-            'edificios.descripcionEdificio',
-            'servicios.descripcionServicio',
-            'unidad_esps.descripcionUnidadEsp',
-            'estado_solicituds.descripcionEstado',
-            'tipo_reparacions.descripcionTipoReparacion',
-            'turnos.descripcionTurno',
-            'users.anexo',
-            DB::raw("IF (users.email IS NULL ,CONCAT('PENDIENTE'), users.email) as email"),
-            'duracion_solicitudes.descripcion_duracion',
-            DB::raw("CONCAT(users.nombre,' ',users.apellido) as nombre"),
-            DB::raw("CONCAT(trabajadores.tra_nombre,' ',trabajadores.tra_apellido) as tra_nombre_apellido"),
-            DB::raw("CONCAT(supervisores.sup_nombre,' ',supervisores.sup_apellido) as sup_nombre_apellido"),
-            DB::raw("CONCAT(solicitud_tickets_e_m_s.id) as nticket"),
-            DB::raw("DATE_FORMAT(solicitud_tickets_e_m_s.created_at, '%d/%m/%Y') as nfechaS"),
-            DB::raw("IF (gestion_ticket_e_m_s.fechaCambiada IS NULL ,DATE_FORMAT(gestion_ticket_e_m_s.fechaInicio, '%d/%m/%Y'), DATE_FORMAT(gestion_ticket_e_m_s.fechaCambiada, '%d/%m/%Y')) as nfechaI"),
-            DB::raw("CONCAT(solicitud_tickets_e_m_s.descripcionP) as desFormat")
-
-        )
-            ->join('trabajadores', 'gestion_ticket_e_m_s.id_trabajador', '=', 'trabajadores.id')
-            ->join('supervisores', 'gestion_ticket_e_m_s.id_supervisor', '=', 'supervisores.id')
-            ->join('solicitud_tickets_e_m_s', 'gestion_ticket_e_m_s.id_solicitud', '=', 'solicitud_tickets_e_m_s.id')
-            ->join('edificios', 'solicitud_tickets_e_m_s.id_edificio', '=', 'edificios.id')
-            ->join('servicios', 'solicitud_tickets_e_m_s.id_servicio', '=', 'servicios.id')
-            ->join('unidad_esps', 'solicitud_tickets_e_m_s.id_ubicacionEx', '=', 'unidad_esps.id')
-            ->join('estado_solicituds', 'solicitud_tickets_e_m_s.id_estado', '=', 'estado_solicituds.id')
-            ->join('tipo_reparacions', 'solicitud_tickets_e_m_s.id_tipoReparacion', '=', 'tipo_reparacions.id')
-            ->join('users', 'solicitud_tickets_e_m_s.id_user', '=', 'users.id')
-            ->join('turnos', 'gestion_ticket_e_m_s.idTurno', '=', 'turnos.id')
-            ->join('duracion_solicitudes', 'gestion_ticket_e_m_s.idDuracion', '=', 'duracion_solicitudes.id')
+        try {
+            $data = GestionTicketEMS::select(
+                'gestion_ticket_e_m_s.*',
+                'edificios.descripcionEdificio',
+                'servicios.descripcionServicio',
+                'unidad_esps.descripcionUnidadEsp',
+                'estado_solicituds.descripcionEstado',
+                'tipo_reparacions.descripcionTipoReparacion',
+                'turnos.descripcionTurno',
+                'users.anexo',
+                DB::raw("IF (users.email IS NULL ,CONCAT('PENDIENTE'), users.email) as email"),
+                'duracion_solicitudes.descripcion_duracion',
+                DB::raw("CONCAT(users.nombre,' ',users.apellido) as nombre"),
+                DB::raw("CONCAT(trabajadores.tra_nombre,' ',trabajadores.tra_apellido) as tra_nombre_apellido"),
+                DB::raw("CONCAT(supervisores.sup_nombre,' ',supervisores.sup_apellido) as sup_nombre_apellido"),
+                DB::raw("CONCAT(solicitud_tickets_e_m_s.id) as nticket"),
+                DB::raw("DATE_FORMAT(solicitud_tickets_e_m_s.created_at, '%d/%m/%Y') as nfechaS"),
+                DB::raw("IF (gestion_ticket_e_m_s.fechaCambiada IS NULL ,DATE_FORMAT(gestion_ticket_e_m_s.fechaInicio, '%d/%m/%Y'), DATE_FORMAT(gestion_ticket_e_m_s.fechaCambiada, '%d/%m/%Y')) as nfechaI"),
+                DB::raw("CONCAT(solicitud_tickets_e_m_s.descripcionP) as desFormat")
+    
+            )
+                ->join('trabajadores', 'gestion_ticket_e_m_s.id_trabajador', '=', 'trabajadores.id')
+                ->join('supervisores', 'gestion_ticket_e_m_s.id_supervisor', '=', 'supervisores.id')
+                ->join('solicitud_tickets_e_m_s', 'gestion_ticket_e_m_s.id_solicitud', '=', 'solicitud_tickets_e_m_s.id')
+                ->join('edificios', 'solicitud_tickets_e_m_s.id_edificio', '=', 'edificios.id')
+                ->join('servicios', 'solicitud_tickets_e_m_s.id_servicio', '=', 'servicios.id')
+                ->join('unidad_esps', 'solicitud_tickets_e_m_s.id_ubicacionEx', '=', 'unidad_esps.id')
+                ->join('estado_solicituds', 'solicitud_tickets_e_m_s.id_estado', '=', 'estado_solicituds.id')
+                ->join('tipo_reparacions', 'solicitud_tickets_e_m_s.id_tipoReparacion', '=', 'tipo_reparacions.id')
+                ->join('users', 'solicitud_tickets_e_m_s.id_user', '=', 'users.id')
+                ->join('turnos', 'gestion_ticket_e_m_s.idTurno', '=', 'turnos.id')
+                ->join('duracion_solicitudes', 'gestion_ticket_e_m_s.idDuracion', '=', 'duracion_solicitudes.id')
+                ->where('gestion_ticket_e_m_s.id_solicitud', $id)
+                ->first();
+    
+            $equipomedico = GestionTicketEMS::select(DB::raw("IF (equipamiento_medicos.equipo IS NULL ,CONCAT('PENDIENTE'), equipamiento_medicos.equipo) as equipo"),
+            DB::raw("IF (equipamiento_medicos.marca IS NULL ,CONCAT('PENDIENTE'), equipamiento_medicos.marca) as marca"),
+            DB::raw("IF (equipamiento_medicos.modelo IS NULL ,CONCAT('PENDIENTE'), equipamiento_medicos.modelo) as modelo"),
+            DB::raw("IF (equipamiento_medicos.serie IS NULL ,CONCAT('PENDIENTE'), equipamiento_medicos.serie) as serie"),
+            DB::raw("IF (equipamiento_medicos.ninventario IS NULL ,CONCAT('PENDIENTE'), equipamiento_medicos.ninventario) as ninventario")) 
+            ->join('tbl_ticket_equipamiento_medicos', 'gestion_ticket_e_m_s.id_solicitud', '=', 'tbl_ticket_equipamiento_medicos.id_solicitud')
+            ->join('equipamiento_medicos', 'tbl_ticket_equipamiento_medicos.id_equipamiento_medico', '=', 'equipamiento_medicos.id')
             ->where('gestion_ticket_e_m_s.id_solicitud', $id)
             ->first();
-
-        $equipomedico = GestionTicketEMS::select(DB::raw("IF (equipamiento_medicos.equipo IS NULL ,CONCAT('PENDIENTE'), equipamiento_medicos.equipo) as equipo"),
-        DB::raw("IF (equipamiento_medicos.marca IS NULL ,CONCAT('PENDIENTE'), equipamiento_medicos.marca) as marca"),
-        DB::raw("IF (equipamiento_medicos.modelo IS NULL ,CONCAT('PENDIENTE'), equipamiento_medicos.modelo) as modelo"),
-        DB::raw("IF (equipamiento_medicos.serie IS NULL ,CONCAT('PENDIENTE'), equipamiento_medicos.serie) as serie"),
-        DB::raw("IF (equipamiento_medicos.ninventario IS NULL ,CONCAT('PENDIENTE'), equipamiento_medicos.ninventario) as ninventario")) 
-        ->join('tbl_ticket_equipamiento_medicos', 'gestion_ticket_e_m_s.id_solicitud', '=', 'tbl_ticket_equipamiento_medicos.id_solicitud')
-        ->join('equipamiento_medicos', 'tbl_ticket_equipamiento_medicos.id_equipamiento_medico', '=', 'equipamiento_medicos.id')
-        ->where('gestion_ticket_e_m_s.id_solicitud', $id)
-        ->first();
-
-        if($equipomedico == ""){
-          $equipomedico = [
-              'equipo' => 'PENDIENTE',
-              'marca' => 'PENDIENTE',
-              'modelo' => 'PENDIENTE',
-              'serie' => 'PENDIENTE',
-              'ninventario' => 'PENDIENTE'
-          ];
-
-          $equipomedico = json_encode($equipomedico);
+    
+            if($equipomedico == ""){
+              $equipomedico = [
+                  'equipo' => 'PENDIENTE',
+                  'marca' => 'PENDIENTE',
+                  'modelo' => 'PENDIENTE',
+                  'serie' => 'PENDIENTE',
+                  'ninventario' => 'PENDIENTE'
+              ];
+    
+              $equipomedico = json_encode($equipomedico);
+            }
+    
+            $listEquipoMedico = $equipomedico;
+    
+            log::info($data);
+    
+            log::info($listEquipoMedico);    
+    
+            $idApoyo1 = $data->idApoyo1;
+            $idApoyo2 = $data->idApoyo2;
+            $idApoyo3 = $data->idApoyo3;
+    
+            $apoyo1 = Trabajadores::firstWhere('id', $idApoyo1);
+            $apoyo2 = Trabajadores::firstWhere('id', $idApoyo2);
+            $apoyo3 = Trabajadores::firstWhere('id', $idApoyo3);
+    
+    
+    
+            $nom1 = "";
+            $ape1 = "";
+            $nom2 = "";
+            $ape2 = "";
+            $nom3 = "";
+            $ape3 = "";
+    
+    
+            if($idApoyo1 == 1){
+                $nom1 = "-";
+                $ape1 = "-";
+            }else{
+                $nom1 = $apoyo1->tra_nombre;
+                    $ape1 = $apoyo1->tra_apellido;
+            }
+    
+            if($idApoyo2 == 1){
+                $nom2 = "-";
+                $ape2 = "-";
+            }else{
+                $nom2 = $apoyo2->tra_nombre;
+                    $ape2 = $apoyo2->tra_apellido;
+            }
+    
+            if($idApoyo3 == 1){
+                $nom3 = "-";
+                $ape3 = "-";
+            }else{
+                $nom3 = $apoyo3->tra_nombre;
+                $ape3 = $apoyo3->tra_apellido;
+            }
+    
+            $idSol = $data->nticket;
+            $nombreTra = $data->tra_nombre_apellido;
+            $nombreSup = $data->sup_nombre_apellido;
+            $desEdificio = $data->descripcionEdificio;
+            $desServicio = $data->descripcionServicio;
+            $desUnidadEsp = $data->descripcionUnidadEsp;
+            $desEstado = $data->descripcionEstado;
+            $desTipoRep = $data->descripcionTipoReparacion;
+            $fechaS = $data->nfechaS;
+            $fechaI = $data->nfechaI;
+            $horasEjecucion = $data->horasEjecucion;
+            $diasEjecucion = $data->diasEjecucion;
+            $descripcionPro = $data->desFormat;
+            $nombreUsuario = $data->nombre;
+            $turno = $data->descripcionTurno;
+            $anexo = $data->anexo;
+            $email = $data->email;
+            $equipo = $listEquipoMedico->equipo;
+            $marca = $listEquipoMedico->marca;
+            $modelo = $listEquipoMedico->modelo;
+            $serie = $listEquipoMedico->serie;
+            $ninventario = $listEquipoMedico->ninventario;
+            $duracionSolicitudes = $data->descripcion_duracion;
+            log::info("Lista final");
+            $data = [
+                'nombreTra' =>  $nombreTra,
+                'idSolicitud' => $idSol,
+                'nombreSup' => $nombreSup,
+                'desEdificio' => $desEdificio,
+                'desServicio' => $desServicio,
+                'desUnidadEsp' => $desUnidadEsp,
+                'desEstado' => $desEstado,
+                'desTipoRep' => $desTipoRep,
+                'fechaS' => $fechaS,
+                'fechaI' => $fechaI,
+                'horasEjecucion' => $horasEjecucion,
+                'diasEjecucion' => $diasEjecucion,
+                'nomApoyo1' => $nom1,
+                'apeApoyo1' => $ape1,
+                'nomApoyo2' => $nom2,
+                'apeApoyo2' => $ape2,
+                'nomApoyo3' => $nom3,
+                'apeApoyo3' => $ape3,
+                'descripcionPro' => $descripcionPro,
+                'nombreUsuario' => $nombreUsuario,
+                'descripcionTurno' =>$turno,
+                'anexo' => $anexo,
+                'duracionSolicitudes' => $duracionSolicitudes,
+                'email' => $email,
+                'equipo' => $equipo,
+                'marca' => $marca,
+                'modelo' => $modelo,
+                'serie' => $serie,
+                'ninventario' => $ninventario
+                
+            ];
+    
+            $pdf = App::make("dompdf.wrapper");
+            $pdf->loadView('TicketEM', $data);
+            $pdf->setOptions(['isJavascriptEnabled' => true]);
+            $pdf->setOptions(['isRemoteEnabled' => true]);
+    
+            return $pdf->stream("TicketEM.pdf", array("Attachment" => 0));
+        } catch (\Throwable $th) {
+            log::info($th);
         }
-
-        $listEquipoMedico = $equipomedico;
-
-        log::info($equipomedico);    
-
-        $idApoyo1 = $data->idApoyo1;
-        $idApoyo2 = $data->idApoyo2;
-        $idApoyo3 = $data->idApoyo3;
-
-        $apoyo1 = Trabajadores::firstWhere('id', $idApoyo1);
-        $apoyo2 = Trabajadores::firstWhere('id', $idApoyo2);
-        $apoyo3 = Trabajadores::firstWhere('id', $idApoyo3);
-
-
-
-        $nom1 = "";
-        $ape1 = "";
-        $nom2 = "";
-        $ape2 = "";
-        $nom3 = "";
-        $ape3 = "";
-
-
-        if($idApoyo1 == 1){
-            $nom1 = "-";
-            $ape1 = "-";
-        }else{
-            $nom1 = $apoyo1->tra_nombre;
-                $ape1 = $apoyo1->tra_apellido;
-        }
-
-        if($idApoyo2 == 1){
-            $nom2 = "-";
-            $ape2 = "-";
-        }else{
-            $nom2 = $apoyo2->tra_nombre;
-                $ape2 = $apoyo2->tra_apellido;
-        }
-
-        if($idApoyo3 == 1){
-            $nom3 = "-";
-            $ape3 = "-";
-        }else{
-            $nom3 = $apoyo3->tra_nombre;
-            $ape3 = $apoyo3->tra_apellido;
-        }
-
-        $idSol = $data->nticket;
-        $nombreTra = $data->tra_nombre_apellido;
-        $nombreSup = $data->sup_nombre_apellido;
-        $desEdificio = $data->descripcionEdificio;
-        $desServicio = $data->descripcionServicio;
-        $desUnidadEsp = $data->descripcionUnidadEsp;
-        $desEstado = $data->descripcionEstado;
-        $desTipoRep = $data->descripcionTipoReparacion;
-        $fechaS = $data->nfechaS;
-        $fechaI = $data->nfechaI;
-        $horasEjecucion = $data->horasEjecucion;
-        $diasEjecucion = $data->diasEjecucion;
-        $descripcionPro = $data->desFormat;
-        $nombreUsuario = $data->nombre;
-        $turno = $data->descripcionTurno;
-        $anexo = $data->anexo;
-        $email = $data->email;
-        $equipo = $listEquipoMedico->equipo;
-        $marca = $listEquipoMedico->marca;
-        $modelo = $listEquipoMedico->modelo;
-        $serie = $listEquipoMedico->serie;
-        $ninventario = $listEquipoMedico->ninventario;
-        $duracionSolicitudes = $data->descripcion_duracion;
-
-        $data = [
-            'nombreTra' =>  $nombreTra,
-            'idSolicitud' => $idSol,
-            'nombreSup' => $nombreSup,
-            'desEdificio' => $desEdificio,
-            'desServicio' => $desServicio,
-            'desUnidadEsp' => $desUnidadEsp,
-            'desEstado' => $desEstado,
-            'desTipoRep' => $desTipoRep,
-            'fechaS' => $fechaS,
-            'fechaI' => $fechaI,
-            'horasEjecucion' => $horasEjecucion,
-            'diasEjecucion' => $diasEjecucion,
-            'nomApoyo1' => $nom1,
-            'apeApoyo1' => $ape1,
-            'nomApoyo2' => $nom2,
-            'apeApoyo2' => $ape2,
-            'nomApoyo3' => $nom3,
-            'apeApoyo3' => $ape3,
-            'descripcionPro' => $descripcionPro,
-            'nombreUsuario' => $nombreUsuario,
-            'descripcionTurno' =>$turno,
-            'anexo' => $anexo,
-            'duracionSolicitudes' => $duracionSolicitudes,
-            'email' => $email,
-            'equipo' => $equipo,
-            'marca' => $marca,
-            'modelo' => $modelo,
-            'serie' => $serie,
-            'ninventario' => $ninventario
-            
-        ];
-
-        $pdf = App::make("dompdf.wrapper");
-        $pdf->loadView('TicketEM', $data);
-        $pdf->setOptions(['isJavascriptEnabled' => true]);
-        $pdf->setOptions(['isRemoteEnabled' => true]);
-
-        return $pdf->stream("TicketEM.pdf", array("Attachment" => 0));
+        
     }
 
     public function imprimirPorTicketIND($id)
