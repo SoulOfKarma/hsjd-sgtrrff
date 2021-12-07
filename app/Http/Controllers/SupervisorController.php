7f1471d4db3ed765c63aafe04cda7603cffa57d4
SoulOfKarma <gomez.soto.ricardo@gmail.com>
2021-12-07 11:15:55 -0300

    ajuste de visualizacion de supervisores

--- app/Http/Controllers/SupervisorController.php
+++ app/Http/Controllers/SupervisorController.php
@@ -18,10 +18,10 @@ class SupervisorController extends Controller
     {
         //Traer todo de supervisores
         $get_all = Supervisores::select('supervisores.id',DB::raw("CONCAT(supervisores.sup_nombre,' ',supervisores.sup_apellido) as sup_nombre_apellido"),'supervisores.id_especialidad1')
-        ->join('tbl_permiso_usuarios','supervisores.sup_run', '=', 'tbl_permiso_usuarios.run_usuario')
+        /* ->join('tbl_permiso_usuarios','supervisores.sup_run', '=', 'tbl_permiso_usuarios.run_usuario')
         ->whereNotIn('supervisores.id_especialidad1',[23])
         ->whereNotIn('supervisores.id_especialidad2',[23])
-        ->whereNotIn('tbl_permiso_usuarios.estado_login',[0])
+        ->whereNotIn('tbl_permiso_usuarios.estado_login',[0]) */
         ->get();
         return  $get_all;
         
